4032335280d314f4bb91ac1134a8b0d0a6652145
Toni Moringa <your-moriil@example.com>
2026-05-17 01:49:49 +0300

    Added components and props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from './components/Header';
+import About from './components/About';
+import ArticleList from './components/ArticleList';
+import blogData from '../data/blog';
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* Header  - blog name as prop */}
+      <Header name={blogData.name} />
+
+      {/* About  - image URL and about text as props */}
+      <About image={blogData.image} about={blogData.about} />
+
+      {/* ArticleList  - array of posts as prop */}
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
